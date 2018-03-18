c46187f2e2be68c7c8e40c198e8b3bba93891ce1
David <dwd7290@yahoo.com>
2018-03-18 20:39:49 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,16 @@
 function forLoop(array) {
     for (let i = 0; i < 25; i++) {
+<<<<<<< HEAD
         if (i <= 1) {
           array[i] = 'I am 1 strange loop.';
         } else if (i > 1) {
           array[i] = `I am ${i} strange loops.`;
+=======
+        if (i === 1) {
+          array[i] = 'I am 1 strange loop.';
+        } else if (i > 1) {
+          array[i] = `I am ${i} strange loop.`;
+>>>>>>> 370193bdb257fafd0a73edeffccd52ba6ee44f94
         }
       }
     return array;
@@ -22,9 +29,19 @@ function doWhileLoop(array) {
   function maybeTrue() {
     return Math.random() >= 0.5;
   }
+<<<<<<< HEAD
   do {
     array = array.slice(0, array.length - 1);  
   } while (array.length > 0 || maybeTrue() === false);
   return array;
 }
 
+=======
+  
+  do {
+    array.slice(0, array.length - 1);  
+  } while (array.length > 0 || maybeTrue() === false);
+  
+  return array;
+}
+>>>>>>> 370193bdb257fafd0a73edeffccd52ba6ee44f94
